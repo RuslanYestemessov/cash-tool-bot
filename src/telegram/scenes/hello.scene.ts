c4be6c27dde7ad1de66676d7a020607680dce222
Ruslan <ruslan.yestemessov@gmail.com>
2022-01-19 03:45:11 +0600

    add more scenario

--- src/telegram/scenes/hello.scene.ts
+++ src/telegram/scenes/hello.scene.ts
@@ -1,20 +1,28 @@
 import { Ctx, On, Scene, SceneEnter } from 'nestjs-telegraf';
-import { HELLO_SCENE } from '../telegram.constants';
+import { HELLO_SCENE } from '../constants/scenes.constants';
 import { Context } from '../interfaces/context.interface';
 import { helloKeyboard } from '../keyboards/hello.keyboard';
+import { HELLO_MESSAGE } from '../constants/messages.constants';
 
 @Scene(HELLO_SCENE)
 export class HelloScene {
   @SceneEnter()
   async onSceneEnter(@Ctx() ctx: Context) {
-    console.log(`${ctx.from.username}: onSceneEnter`);
-    await ctx.telegram.sendMessage(ctx.chat.id, '123', {
+    console.log(`${ctx.from.username} entered ${HELLO_SCENE}`);
+    await ctx.telegram.sendMessage(ctx.chat.id, HELLO_MESSAGE, {
       reply_markup: helloKeyboard
     });
   }
 
   @On('contact')
-  authorization(@Ctx() ctx: Context) {
-    return JSON.stringify(ctx.message);
+  async authorization(@Ctx() ctx: Context) {
+    await ctx.scene.leave();
+    await ctx.telegram.sendMessage(ctx.chat.id, 'Вы не авторизованы', {
+      allow_sending_without_reply: true,
+      reply_markup: {
+        remove_keyboard: true
+      }
+    });
+    console.log(`${ctx.from.username} authorized`);
   }
 }
--- src/telegram/telegram.update.ts
+++ src/telegram/telegram.update.ts
@@ -1,11 +1,16 @@
 import { Ctx, Start, Update } from 'nestjs-telegraf';
 import { Context } from './interfaces/context.interface';
-import { HELLO_SCENE } from './telegram.constants';
+import { HELLO_SCENE } from './constants/scenes.constants';
+import { START_COMMAND_ERROR } from './constants/messages.constants';
 
 @Update()
 export class TelegramUpdate {
   @Start()
   start(@Ctx() ctx: Context) {
-    ctx.scene.enter(HELLO_SCENE);
+    if (!ctx.scene?.current?.id && ctx.scene?.current?.id !== HELLO_SCENE) {
+      ctx.scene.enter(HELLO_SCENE);
+    } else {
+      return START_COMMAND_ERROR;
+    }
   }
 }
